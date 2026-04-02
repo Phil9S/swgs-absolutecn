95ef1b497b070dd3a78e309e59adc2d0e6b30289
Phil9S <philip.smith@cruk.cam.ac.uk>
2026-04-02 18:01:22 +0100

    complete update to snakemake v9 or greater, reimplementation of cluster profiles to match updated version, fixed schema depreciation warnings, simplified environment install process, removed unused update_config.py script, introduced pixi toml and lock files for better env management, renamed and moved a nubmer of files to more intuitive locations.

--- resources/package_load.R
+++ resources/package_load.R
@@ -1,14 +1,14 @@
 # Check if packages are installed
-listOfAllPackages = c("Biobase","dplyr","tidyverse","QDNAseq","doMC","QDNAseq",
-	"QDNAseqmod","QDNAseq.hg19","QDNAseq.hg38","foreach","ggplot2","stringr",
-	"matrixStats","parallel","plyr")
+listOfAllPackages = c("Biobase","tidyverse","QDNAseq",
+	"QDNAseqmod","QDNAseq.hg19","QDNAseq.hg38",
+	"matrixStats","parallel","plyr","ranger","parsnip")
 
 for(thisPackage in listOfAllPackages) {
   
   if(thisPackage %in% rownames(installed.packages()) == FALSE) {
-	cat(paste("[install_env] ERROR - Package", thisPackage, "needs installing.\n"))
+	cat(paste("[install_extra_env][check_packages] ERROR - Package", thisPackage, "needs installing.\n"))
   } else {
-	cat(paste("[install_env] Package", thisPackage, "is installed.\n"))
+	cat(paste("[install_extra_env][check_packages] Package", thisPackage, "is installed.\n"))
   }
 }
 #a <- lapply(listOfAllPackages,FUN = function(x){suppressPackageStartupMessages(require(x, character.only = TRUE))})
