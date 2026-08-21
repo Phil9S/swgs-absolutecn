26e2130fa6d8b9d6188d09621aa5ecb54d051367
Philip Smith <philip.smith@cruk.cam.ac.uk>
2026-08-21 15:48:22 +0100

    removed hardcoded parameters and sample inputs from workflow yaml. Included example command for argo deployment

--- resources/deployArgo.sh
+++ resources/deployArgo.sh
@@ -1,3 +1,5 @@
 minikube start
 nohup minikube mount /home/psmith/swgs-absolutecn/data/:/mnt/data/ &
-argo submit -n argo --watch workflow/argo-auto-workflow.yaml
+argo submit -n argo --watch workflow/argo-auto-workflow.yaml \
+	-f config/config.yaml \
+	-p samples='[{"sampleId":"SAMPLE_1","bam":"/mnt/data/RMNISTHS_0_point_1xdownsample.bam"},{"sampleId":"SAMPLE_2","bam":"/mnt/data/RMNISTHS_0_point_1xdownsample.bam"}]'
